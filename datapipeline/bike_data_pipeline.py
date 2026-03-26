097dd1739b9f65831d18fe34f9cc9ed6abb826e4
Sagar Vedantam <vedantsd@mail.uc.edu>
2026-03-26 19:27:13 +0000

    Added coding comments in the code

--- datapipeline/bike_data_pipeline.py
+++ datapipeline/bike_data_pipeline.py
@@ -49,6 +49,12 @@ def validate_data(df: pd.DataFrame, is_train: bool = True) -> None:
 # ------------------------------------------------------
 # 3. Feature Engineering
 # ------------------------------------------------------
+
+# Machine‑learning models don’t understand timestamps directly.
+# They do understand numeric patterns like “demand is higher on weekdays” or 
+# “evenings are busier”.
+# This function converts a timestamp into those signals.
+
 def add_time_features(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
     df["hour"] = df["datetime"].dt.hour
@@ -57,6 +63,8 @@ def add_time_features(df: pd.DataFrame) -> pd.DataFrame:
     df["is_weekend"] = df["dayofweek"].isin([5, 6]).astype(int)
     return df
 
+# Bike demand at time t is often strongly related to demand at time t‑1.
+# Lag features encode this “momentum” effect.
 
 def add_lag_feature(df: pd.DataFrame, is_train: bool) -> pd.DataFrame:
     df = df.sort_values("datetime")
@@ -73,6 +81,14 @@ def add_lag_feature(df: pd.DataFrame, is_train: bool) -> pd.DataFrame:
 # ------------------------------------------------------
 # 4. Preprocessing
 # ------------------------------------------------------
+
+# Numeric (e.g., temperature, hour)
+# Categorical (e.g., season, weather)
+# This function builds a single, unified preprocessing object that:
+# scales numeric features,
+# one‑hot encodes categorical features,
+# applies the same logic to train and test data.
+
 def build_preprocessor():
     categorical_features = [
         "season", "holiday", "workingday", "weather", "is_weekend"
@@ -94,6 +110,14 @@ def build_preprocessor():
 # ------------------------------------------------------
 # 5. Full Pipeline
 # ------------------------------------------------------
+
+# Numerical Features temp, atemp (feels like), humidity, windspeed, hour, dayofweek, month, lag_1_hour (Bike count from previous hour)
+# Categorical Features season (1,2,3,4 - 4 seasons), holiday (0,1 binary is it a holiday), 
+#             workingday (0,1 is it a working day),  is_weekend (0,1 is it weekend) 
+#             weather (1 - Clear, clear to partly cloudy, 2 - Mist, cloudy, 3 - Light rain or snow, 4 - Heavy rain or snow, severe conditions)                            
+# Total Numerical Features - 8
+# Total Categorical Features - 14 (sum up all the values, 4 seaons, 2 binary for holiday, 2 binary for working day, 2 binary for week end, 4 weather conditions)
+# Total Features: 8 + 14 = 22
 def run_training_pipeline():
     df = load_data(TRAIN_PATH)
     validate_data(df, is_train=True)
